c7d0b63d43c33223a5a20d2d9daa5dfbcb99354e
Geoffrey Mainland <mainland@apeiron.net>
2023-01-18 15:59:16 -0500

    Initial import.

---
+++ more-scheme.rkt
@@ -0,0 +1,40 @@
+;;
+;; DO NOT REMOVE THESE TWO LINES
+;;
+#lang racket
+(provide (all-defined-out))
+
+;;
+;; Problem 1
+;;
+
+(define (stream-pair-with f s)
+  'not-implemented)
+
+;;
+;; Problem 2
+;;
+
+(define (stream-iterate f x)
+  'not-implemented)
+
+;;
+;; Problem 3
+;;
+
+(define (stream-zip xs ys)
+  'not-implemented)
+
+;;
+;; Problem 4
+;;
+
+(define (cycle xs)
+  'not-implemented)
+
+;;
+;; Problem 5
+;;
+
+(define apply-count
+  'not-implemented)
